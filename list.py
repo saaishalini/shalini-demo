667a7d4784700e19ea35ab114e1aefbb0c2dd4d5
saaishalini <92154646+saaishalini@users.noreply.github.com>
2023-04-26 18:21:25 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,16 @@
+#creating a list
+students=["shalini","saranya","priya","sangeetha"]
+print(students)
+
+#print using index value
+print(students[3])
+#slicing method
+print(students[:2])
+
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
